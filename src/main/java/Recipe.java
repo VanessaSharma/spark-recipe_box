8988c44abb26f84d0e879e72fe3c0a81806d4b7b
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-28 10:36:42 -0700

    Add getters and variables to Recipe.java.

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,10 +1,44 @@
 import org.sql2o.*;
 import java.util.List;
 
-public class  {
+public class Recipe {
+  public String name;
+  public String description;
+  public String ingredient;
+  public String instruction;
+  public int rating;
+  public int id;
 
 
-  public () {
-
+  public Recipe(String name, String description, String ingredient, String instruction, int rating) {
+    this.name = name;
+    this.description = description;
+    this.ingredient = ingredient;
+    this.instruction = instruction;
+    this.rating = rating;
   }
-  
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getIngredient() {
+    return ingredient;
+  }
+
+  public String getInstruction() {
+    return instruction;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public int getId() {
+    return id;
+  }
+}
